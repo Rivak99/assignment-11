db02590dcdc1e5840843aff2565fef7826b768f6
Rivak99 <rivak999@gmail.com>
2026-08-31 11:52:31 +0300

    assignment 11 added

---
+++ scripts/index.js
@@ -0,0 +1,5 @@
+const username = "Hussein Droubi";
+let age = 29;
+let year = 2;
+console.log(`My name is ${username}. I'm ${age} years old`);
+console.log(`${year} years ago, I was ${(age -= year)} years old`);
